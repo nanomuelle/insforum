a8e332e8622d74be5a0f4949e59b75e89ca36f23
Fernando Garcia Huerta <nanomuelle@gmail.com>
2016-08-13 13:44:24 +0200

    Added test for over sized and over resolution images

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,25 +15,62 @@ class DefaultControllerTest extends WebTestCase
     * -> no post added
     * -> no views increment
     */
-    public function testEmptyForm() {
+    // public function testEmptyForm() {
+    //     $client = static::createClient();
+    //     $crawler = $client->request('GET', '/');
+    //
+    //     $numPostsText = $crawler->filter('#numPosts')->text();
+    //     $numViewsText = $crawler->filter('#numViews')->text();
+    //
+    //     $form = $crawler->selectButton('Send')->form();
+    //     $crawler = $client->submit($form);
+    //
+    //     $this->assertEquals(
+    //         $numPostsText,
+    //         $crawler->filter('#numPosts')->text()
+    //     );
+    //
+    //     $this->assertEquals(
+    //         $numViewsText,
+    //         $crawler->filter('#numViews')->text()
+    //     );
+    // }
+
+    public function testOverResolutionImage() {
+        $imageName = 'over_resolution_image.jpg';
+        $imagePath = __DIR__ . '/../../Resources/testImages/';
+        $uploadedImage = new UploadedFile($imagePath . $imageName, $imageName);
+
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        $numPostsText = $crawler->filter('#numPosts')->text();
-        $numViewsText = $crawler->filter('#numViews')->text();
+        $form = $crawler->selectButton('Send')->form();
+        $form['post[title]'] = "Over resolution image test";
+        $form['post[imageFile]']->upload($imagePath . $imageName);
+
+        $crawler = $client->submit($form);
+        $this->assertEquals(
+            1,
+            $crawler->filter('html:contains("The image width is too big")')->count());
+    }
+
+    public function testOverSizedImage() {
+        $imageName = 'more_than_2M_image.jpg';
+        $imagePath = __DIR__ . '/../../Resources/testImages/';
+        $uploadedImage = new UploadedFile($imagePath . $imageName, $imageName);
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
 
         $form = $crawler->selectButton('Send')->form();
+        $form['post[title]'] = "Over sized image test";
+        $form['post[imageFile]']->upload($imagePath . $imageName);
+
         $crawler = $client->submit($form);
-
         $this->assertEquals(
-            $numPostsText,
-            $crawler->filter('#numPosts')->text()
-        );
+            1,
+            $crawler->filter('html:contains("The file is too large")')->count());
 
-        $this->assertEquals(
-            $numViewsText,
-            $crawler->filter('#numViews')->text()
-        );
     }
 
     // public function testValidPngImage() {
