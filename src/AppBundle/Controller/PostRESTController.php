531aa4d6bfa488cc07f03563e8d2e59c3969357a
Fernando Garcia Huerta <nanomuelle@gmail.com>
2016-08-14 00:38:44 +0200

    Implemented basic API

--- src/AppBundle/Controller/PostRESTController.php
+++ src/AppBundle/Controller/PostRESTController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
-use AppBundle\Form\RestPostType;
+use AppBundle\Form\PostType;
 
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
@@ -26,6 +26,18 @@ use Voryx\RESTGeneratorBundle\Controller\VoryxController;
 class PostRESTController extends VoryxController
 {
     /**
+    * API Get number of Posts
+    */
+    public function countAction() {
+        $numPosts = $this->getDoctrine()->getRepository(Post::class)
+            ->getCount();
+
+        return array(
+            'numPosts' => $numPosts
+        );
+    }
+
+    /**
      * Get a Post entity
      *
      * @View(serializerEnableMaxDepthChecks=true)
@@ -46,21 +58,12 @@ class PostRESTController extends VoryxController
      *
      * @return Response
      *
-     * @QueryParam(name="offset", requirements="\d+", nullable=true, description="Offset from which to start listing notes.")
-     * @QueryParam(name="limit", requirements="\d+", default="20", description="How many notes to return.")
-     * @QueryParam(name="order_by", nullable=true, array=true, description="Order by fields. Must be an array ie. &order_by[name]=ASC&order_by[description]=DESC")
-     * @QueryParam(name="filters", nullable=true, array=true, description="Filter by fields. Must be an array ie. &filters[id]=3")
      */
     public function cgetAction(ParamFetcherInterface $paramFetcher)
     {
         try {
-            $offset = $paramFetcher->get('offset');
-            $limit = $paramFetcher->get('limit');
-            $order_by = $paramFetcher->get('order_by');
-            $filters = !is_null($paramFetcher->get('filters')) ? $paramFetcher->get('filters') : array();
-
             $em = $this->getDoctrine()->getManager();
-            $entities = $em->getRepository('AppBundle:Post')->findBy($filters, $order_by, $limit, $offset);
+            $entities = $em->getRepository('AppBundle:Post')->findAllRecentFirst();
             if ($entities) {
                 return $entities;
             }
@@ -83,11 +86,13 @@ class PostRESTController extends VoryxController
     public function postAction(Request $request)
     {
         $entity = new Post();
-        $form = $this->createForm(get_class(new RestPostType()), $entity, array("method" => $request->getMethod()));
+        $form = $this->createForm(get_class(new PostType()), $entity, array("method" => $request->getMethod()));
         $this->removeExtraFields($request, $form);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            $entity->setImageOriginalName($entity->getImageFile()->getClientOriginalName());
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
@@ -97,49 +102,7 @@ class PostRESTController extends VoryxController
 
         return FOSView::create(array('errors' => $form->getErrors()), Codes::HTTP_INTERNAL_SERVER_ERROR);
     }
-    /**
-     * Update a Post entity.
-     *
-     * @View(serializerEnableMaxDepthChecks=true)
-     *
-     * @param Request $request
-     * @param $entity
-     *
-     * @return Response
-     */
-    public function putAction(Request $request, Post $entity)
-    {
-        try {
-            $em = $this->getDoctrine()->getManager();
-            $request->setMethod('PATCH'); //Treat all PUTs as PATCH
-            $form = $this->createForm(get_class(new RestPostType()), $entity, array("method" => $request->getMethod()));
-            $this->removeExtraFields($request, $form);
-            $form->handleRequest($request);
-            if ($form->isValid()) {
-                $em->flush();
 
-                return $entity;
-            }
-
-            return FOSView::create(array('errors' => $form->getErrors()), Codes::HTTP_INTERNAL_SERVER_ERROR);
-        } catch (\Exception $e) {
-            return FOSView::create($e->getMessage(), Codes::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-    /**
-     * Partial Update to a Post entity.
-     *
-     * @View(serializerEnableMaxDepthChecks=true)
-     *
-     * @param Request $request
-     * @param $entity
-     *
-     * @return Response
-     */
-    public function patchAction(Request $request, Post $entity)
-    {
-        return $this->putAction($request, $entity);
-    }
     /**
      * Delete a Post entity.
      *
